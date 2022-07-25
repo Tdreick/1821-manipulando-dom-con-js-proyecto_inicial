5123d6c462300c4b474eb1645a43ba3cbfea21ea
Tdreick <tdreick@gmail.com>
2022-07-25 00:18:13 -0600

    clase04 completada

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+(() => {
 const btn = document.querySelector('[data-form-btn]');
 const createTask = (evento) =>{
     evento.preventDefault();
@@ -7,18 +8,35 @@ const createTask = (evento) =>{
     const task = document.createElement("li");
     task.classList.add('card');
     input.value = '';
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
+    //backticks ``
+    
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask =document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
   <i class="fas fa-trash-alt trashIcon icon"></i>`
-  task.innerHTML = content;
-
-  list.appendChild(task);
-    console.log(content);
-} 
-console.log(btn);
+  //task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
+};
 //arrows o funciones anonimas
 btn.addEventListener("click", createTask );
 
+const checkComplete =() =>{
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon" );
+    i.addEventListener("click", completeTask);
+    return i;
+};
+//inmediately invoked function expression "IIFE" (()=>{conjunto de expresion}) y se llama inmediatamente con ();
+const completeTask = (event) =>{
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+};
+})();
 
